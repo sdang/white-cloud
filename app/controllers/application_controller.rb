9147b5c30cce498a81b20ccd0a5bd6198b21763b
Sabin Dang <sabin@sabindang.com>
2012-01-29 10:45:30 -0800

    basic 2 column layout with bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if user_signed_in?
+      "application"
+    else
+      "devise"
+    end
+  end
 end
