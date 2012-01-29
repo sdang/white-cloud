b54d78c07812ab7b83b2d1e8ee9bf09f5d045d41
Sabin Dang <sabin@sabindang.com>
2012-01-28 23:44:38 -0800

    added devise user authentication to application

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module OliveViewTools
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # For DEVISE
+    config.assets.initialize_on_precompile = false
   end
 end
