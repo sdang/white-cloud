f6f511ab790d2654d109f175df63188cfaba5d24
Sabin Dang <sabin@sabindang.com>
2012-02-08 13:04:27 -0800

    added encrypted phi database fields, basic controller for dc summaries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,20 @@ class ApplicationController < ActionController::Base
       "devise"
     end
   end
+  
+  private
+  def authenticate_user!
+    if user_signed_in?
+      # check if authorized
+      unless current_user.authorized?
+        reset_session
+        flash[:alert] = "You have not been authorized to use this site, please contact the department of medicine for approval"
+        redirect_to(:action=>"new", :controller=>"/devise/sessions")
+      end
+    else
+      redirect_to(:action=>"new", :controller=>"/devise/sessions") unless user_signed_in?
+    end
+  end
+  
+  
 end
--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,5 +1,5 @@
 class RemindersController < ApplicationController
-  
+  before_filter :authenticate_user!
   def index
   end
   
