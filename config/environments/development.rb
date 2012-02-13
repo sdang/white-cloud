a77b7d60d9839f448c09c3298c119e1d3370b354
Sabin Dang <sabin@sabindang.com>
2012-02-13 03:13:35 -0800

    fixed mailer to properly reflect sendgrid for production apps

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,17 +27,6 @@ OliveViewTools::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  
-  # set URL Path
-  config.action_mailer.default_url_options = { :host => "olive-view-tools.dev" }
-  
-  # email configuration
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    :address              => "localhost",
-    :port                 => 1025,
-    :domain               => ENV['SEND_GRID_DOMAIN']
-  }
+
   
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,6 @@ OliveViewTools::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
-  config.action_mailer.default_url_options = { :host => ENV['DEFAULT_HOST'] }
+
   
 end
