bc07ccb32873097e2aad76d66085f174c85ef830
Sabin Dang <sabin@sabindang.com>
2012-02-20 14:22:06 -0800

    added more tests to validate models

--- test/functional/reminder_lists_controller_test.rb
+++ test/functional/reminder_lists_controller_test.rb
@@ -4,4 +4,21 @@ class ReminderListsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "create without user id" do
+    rl = ReminderList.new(:name => "test")
+    assert !rl.save, "saved reminder list without assigning it to a supervisor"
+  end
+  
+  test "create without name" do
+    rl = ReminderList.new(:user_id => 1)
+    assert !rl.save, "saved reminder list without a name"
+  end
+  
+  test "create two with same name" do
+    rl = ReminderList.new(:name => "test", :user_id => 1)
+    rl.save
+    rl2 = ReminderList.new(:name => "test", :user_id => 1)
+    assert !rl.save, "saved two reminder lists with the same name"
+  end
 end
