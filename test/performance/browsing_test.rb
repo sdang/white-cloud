8a466efd683d68d978a48e0ecbe1d2b5c1886576
Sabin Dang <sabin@sabindang.com>
2012-02-22 15:15:03 -0800

    improved testing

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -18,18 +18,5 @@ class BrowsingTest < ActionDispatch::PerformanceTest
     get "/admin/dashboard"
     assert_response :success
   end
-  
-  def test_edit_dc_summary
-    get "/users/sign_in"
-    assert_response :success
- 
-    post_via_redirect "/users/sign_in", :user => {:email => users(:one).email, :password => "fdxfdx1"}
-    
-    get "/dc_summaries/#{dc_summaries(:basic).id}/edit"
-    
-    # unlock phi
-    post_via_redirect "/phi/unlock_phi", :group_password => ENV["GROUP_PW"]
-    assert_response :success
-  end
     
 end
