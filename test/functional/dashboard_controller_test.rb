b5d9f6cccf384f6e123171ba8f421625eaf6c333
Sabin Dang <sabin@sabindang.com>
2012-02-22 11:37:14 -0800

    working devise compat controller testing

--- test/functional/dashboard_controller_test.rb
+++ test/functional/dashboard_controller_test.rb
@@ -1,15 +1,29 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  
+  include Devise::TestHelpers
+
   test "should redirect to login" do
     get :index
     assert_redirected_to :action=>"new", :controller=>"/devise/sessions"
 
     get :search
+    assert_redirected_to :action=>"new", :controller=>"/devise/sessions"
+  end
+  
+  test "should render login" do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in User.find(users(:two).id)
+    
+    get :index
+    assert_response :success, "Could not load dashboard#index"
+  end
+  
+  test "should redirect non-authorized user to login" do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in User.find(users(:three).id)
+    
+    get :index
     assert_redirected_to :action=>"new", :controller=>"/devise/sessions"
   end
   
