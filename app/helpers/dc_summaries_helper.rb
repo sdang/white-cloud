089260952286a06c362ee9317c45cd3db6561597
Sabin Dang <sabin@sabindang.com>
2012-03-03 00:13:05 -0800

    shrink amount of info returned on import fixes #31

--- app/helpers/dc_summaries_helper.rb
+++ app/helpers/dc_summaries_helper.rb
@@ -3,7 +3,7 @@ module DcSummariesHelper
     html = ""
     # html += button_tag "import from signout", :class => "button white small", :id => "#{prefix}-import-signout"
     # html += " "
-    html += "<div id='#{prefix}-import-error' class='hidden alert'></div><div id='#{prefix}-import-notice' class='hidden notice'></div>"
+    html += "<div id='#{prefix}-import-error' class='hidden alert alert-error'></div><div id='#{prefix}-import-notice' class='hidden alert alert-success'></div>"
     html += link_to "import from last d/c summary", url_for(:controller => "/dc_summaries", :action => "import_handler", :prefix => prefix, :from => "dc", :id => dc_summary_id), :class => "btn btn-mini", :remote => true, :id => "#{prefix}-import-dc-summary"
     return html.html_safe
   end
